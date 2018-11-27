ee0d816ad3041b87fd8e3231c00e753e2e893533
Tatsuya Ishibashi <e185714@ie.u-ryukyu.ac.jp>
2018-11-27 13:58:50 +0900

    1st commit

---
+++ src/main/java/jp/ac/uryukyu/ie/e185714/Enemy.java
@@ -0,0 +1,24 @@
+package jp.ac.uryukyu.ie.e185714;
+
+
+public class Enemy extends LivingThing {
+
+    public Enemy (String name, int maximumHP, int attack) {
+        super(name, maximumHP, attack);
+    }
+
+    /**
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。
+     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * @param damage 受けたダメージ
+     */
+
+    //@override
+    public void wounded(int damage) {
+        setHitPoint(getHitPoint() - damage);
+        if( getHitPoint() < 0 ) {
+            setDead(true);
+            System.out.printf("モンスター%sは倒れた。\n", getName());
+        }
+    }
+}
